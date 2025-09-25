dbc201a7aa79d567390fd6b93c3815e9f8ce9c74
cattenlinger <cattenlinger@outlook.com>
2025-09-25 11:37:58 +0800

    Try improve loader codes

--- src/huzpsb/ll4j/data/CsvLoader.java
+++ src/huzpsb/ll4j/data/CsvLoader.java
@@ -1,26 +1,26 @@
 package huzpsb.ll4j.data;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class CsvLoader {
     public static DataSet load(String path, int labelIndex) {
         try {
-            DataSet data = new DataSet();
-            Scanner sc = new Scanner(new File(path), "UTF-8");
-            String[] header = sc.nextLine().split(",");
+            final DataSet data = new DataSet();
+            final Scanner sc = new Scanner(new File(path), StandardCharsets.UTF_8);
+            final String[] header = sc.nextLine().split(",");
             int n = header.length;
             while (sc.hasNextLine()) {
-                String[] line = sc.nextLine().split(",");
-                double[] x = new double[n - 1];
-                int label = Integer.parseInt(line[labelIndex]);
+                final String[] line = sc.nextLine().split(",");
+                final double[] x = new double[n - 1];
+                final int label = Integer.parseInt(line[labelIndex]);
                 int idx = 0;
                 for (int i = 0; i < n; i++) {
-                    if (i != labelIndex) {
-                        x[idx++] = Double.parseDouble(line[i]);
-                    }
+                    if (i == labelIndex) continue;
+                    x[idx++] = Double.parseDouble(line[i]);
                 }
-                DataEntry entry = new DataEntry(label, x);
+                final DataEntry entry = new DataEntry(label, x);
                 data.split.add(entry);
             }
             return data;
