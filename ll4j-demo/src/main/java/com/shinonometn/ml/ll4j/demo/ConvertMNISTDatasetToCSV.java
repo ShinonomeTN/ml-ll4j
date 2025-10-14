c681e5ffae64dceef73ac556ac5e107f113b1f6c
cattenlinger <cattenlinger@outlook.com>
2025-10-15 01:10:32 +0800

    Add MNIST converter

---
+++ ll4j-demo/src/main/java/com/shinonometn/ml/ll4j/demo/ConvertMNISTDatasetToCSV.java
@@ -0,0 +1,86 @@
+package com.shinonometn.ml.ll4j.demo;
+
+import com.shinonometn.tools.MNIST;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ConvertMNISTDatasetToCSV {
+    /* MNIST Training Label Set */
+    private final static String LABEL_PATH = "./digits/t10k-labels-idx1-ubyte";
+    /* MNIST Training Image Data */
+    private final static String IMAGE_PATH = "./digits/t10k-images-idx3-ubyte";
+    /* CSV Output Path */
+    private final static String OUTPUT_PATH = "./digits/test-images.csv";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Label Path  : " + LABEL_PATH);
+        System.out.println("Image Path  : " + IMAGE_PATH);
+        System.out.println("Output Path : " + OUTPUT_PATH);
+
+        System.out.println("Convert MNIST Data set to CSV...");
+
+        // Load labels
+        System.out.println("Open labels...");
+        final MNIST.EntryIterator labelIterator = createDataSetIterator(Paths.get(LABEL_PATH));
+        final int[] labels = new int[labelIterator.entryCount];
+        int labelIndex = 0;
+        for (MNIST.Entry it = labelIterator.next(); labelIterator.hasNext(); it = labelIterator.next()) {
+            labels[labelIndex++] = it.data[0];
+        }
+        labelIterator.close();
+        System.out.printf("Label Count: %d%n", labelIndex + 1);
+
+        // Load images
+        System.out.println("Open images...");
+        final MNIST.EntryIterator imageIterator = createDataSetIterator(Paths.get(IMAGE_PATH));
+
+        /* Write CSV header */
+        final PrintWriter writer = new PrintWriter(OUTPUT_PATH);
+        writer.print("label");
+        for (int i = 0; i < imageIterator.getEntrySize(); i++) writer.print(",pixel" + i);
+        writer.println();
+        writer.flush();
+        System.out.println("CSV header written.");
+
+        /* Write images */
+        int imageIndex = 0;
+        for (MNIST.Entry it = imageIterator.next(); imageIterator.hasNext(); it = imageIterator.next()) {
+            final int label = labels[imageIndex];
+            final int w = it.getDimensionSize(0);
+            final int h = it.getDimensionSize(1);
+            final byte[] data = it.data;
+            writer.print(label);
+            for (int x = 0; x < w; x++) {
+                for (int y = 0; y < h; y++) {
+                    // normalize
+                    final double value = (double) (data[x * h + y] & 0xFF) / 255;
+                    writer.write("," + value);
+                }
+            }
+            writer.println();
+            imageIndex++;
+        }
+        if (imageIndex != labelIndex) System.out.println(">> Warning: label count != image count <<");
+        writer.flush();
+        writer.close();
+        System.out.println("File saved. total " + (imageIndex + 1) + " images.");
+    }
+
+    //================================================================
+    private static MNIST.EntryIterator createDataSetIterator(final Path path) throws IOException {
+        final MNIST.EntryIterator iterator = MNIST.createDataSetIterator(path);
+        System.out.printf(
+                "Data type: 0x%02x %s, dimensions: %d%n",
+                iterator.type.code,
+                iterator.type.description,
+                iterator.dataSetDimensions
+        );
+        for (int i = 0; i < iterator.getEntryDimensions(); i++) {
+            System.out.printf("Entry Dimension %d size: %d%n", i + 1, iterator.getEntryDimensionSize(i));
+        }
+        return iterator;
+    }
+}
---
+++ ll4j-demo/src/main/java/com/shinonometn/tools/MNIST.java
@@ -0,0 +1,179 @@
+package com.shinonometn.tools;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * MNIST database dataset tools
+ * <p>
+ *
+ * Handling MNIST dataset file format.
+ * See: <a href="https://github.com/cvdfoundation/mnist?tab=readme-ov-file">GitHub - cvdfoundation/mnist</a>
+ */
+public final class MNIST {
+    private MNIST() {
+    }
+
+    //================================================================
+
+    public static EntryIterator createDataSetIterator(Path filePath) throws IOException {
+        final InputStream inputStream = Files.newInputStream(filePath);
+
+        // Read the magic
+        final int[] magic = new int[4];
+        for (int i = 0; i < magic.length; i++) {
+            magic[i] = inputStream.read();
+        }
+
+        // Check the magic number
+        final int magicSum = magic[0] + magic[1];
+        if (magic[0] + magic[1] != 0) throw new IllegalArgumentException("Invalid magic sum of file: " + magicSum);
+
+        // Read dimension sizes
+        final int[] sizes = new int[magic[3]];
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = nextInt(inputStream, 4);
+        }
+
+        return new EntryIterator(getType(magic[2]), sizes, inputStream);
+    }
+
+    public static class EntryIterator implements Closeable, Iterator<Entry> {
+        /** File data unit type */
+        public final DataType type;
+        /** Total dimension of this file */
+        public final int dataSetDimensions;
+        /** Entry count described in file meta, same as the size of the first dimension. */
+        public final int entryCount;
+        /** Entry data length */
+        final int entrySize;
+
+        private final int[] dimensionSizes;
+        private final InputStream in;
+
+        EntryIterator(final DataType type, final int[] dimensionSizes, final InputStream in) {
+            this.type = type;
+            this.dataSetDimensions = dimensionSizes.length;
+
+            this.entrySize = (dataSetDimensions > 1)
+                    ? Arrays.stream(dimensionSizes)
+                            .skip(1)
+                            .map(s -> s * type.size)
+                            .reduce(1, (a, b) -> a * b)
+                    : type.size; /* size * 1 */
+
+            if (dataSetDimensions > 1) {
+                this.dimensionSizes = new int[dataSetDimensions - 1];
+                System.arraycopy(dimensionSizes, 1, this.dimensionSizes, 0, dataSetDimensions - 1);
+            } else {
+                this.dimensionSizes = new int[]{type.size};
+            }
+
+            this.in = in;
+            this.entryCount = dimensionSizes[0];
+        }
+
+        public int getEntryDimensionSize(final int index) {
+            return dimensionSizes[index];
+        }
+
+        public int getEntryDimensions() {
+            return dimensionSizes.length;
+        }
+
+        /** Get the product of entry dimensions */
+        public int getEntrySize() {
+            return Arrays.stream(dimensionSizes).reduce(1, (a, b) -> a * b);
+        }
+
+        @Override
+        public void close() throws IOException {
+            in.close();
+        }
+
+        private int yieldIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return yieldIndex < entryCount;
+        }
+
+        @Override
+        public Entry next() {
+            try {
+                final byte[] data = new byte[entrySize];
+                final int read = in.read(data, 0, entrySize);
+                if (read != entrySize) throw new EOFException("Unexpected end of source stream");
+                final Entry entry = new Entry(dimensionSizes, data);
+                yieldIndex++;
+                return entry;
+            } catch (Exception e) {
+                /* Throw all unexpected exceptions. */
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static class Entry {
+        private final int[] sizes;
+        public final byte[] data;
+
+        /** How many dimensions of this entry */
+        public int getDimensions() {
+            return sizes.length;
+        }
+
+        /** Product of dimensions */
+        public int getDimensionSize(final int index) {
+            return sizes[index];
+        }
+
+        Entry(final int[] sizes, final byte[] data) {
+            this.sizes = sizes;
+            this.data = data;
+        }
+    }
+
+    //================================================================
+
+    public enum DataType {
+        /* 0x08 */ UByte("unsigned byte", 1, 0x08),
+        /* 0x09 */ Byte("signed byte", 1, 0x09),
+        /* 0x0A */ Reserved("reserved (unused)", -1, 0x0A),
+        /* 0x0B */ Short("short (2 bytes)", 2, 0x0B),
+        /* 0x0C */ Int("int (4 bytes)", 4, 0x0C),
+        /* 0x0D */ Float("float (4 bytes)", 4, 0x0D),
+        /* 0x0E */ Double("double (8 bytes)", 8, 0x0E);
+
+        public final String description;
+        public final int size;
+        public final int code;
+
+        DataType(final String description, final int size, int code) {
+            this.description = description;
+            this.size = size;
+            this.code = code;
+        }
+    }
+
+    static DataType getType(final int type) {
+        final int index = type - 8;
+        if (index >= DataType.values().length) throw new IllegalArgumentException("Invalid type code: " + type);
+        return DataType.values()[index];
+    }
+
+    //================================================================
+
+    /** Read next int from input stream */
+    private static int nextInt(final InputStream is, final int bytes) throws IOException {
+        int result = 0;
+        for (int i = 0; i < bytes; i++) {
+            result <<= 8;
+            result |= (is.read() & 0xFF);
+        }
+        return result;
+    }
+}
